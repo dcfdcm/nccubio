413f3bacd4d5c6b05180ca79bd2859517f3682a5
dcfdcm <dcfdcm@gmail.com>
2016-11-08 22:38:00 +0800

    Add files via upload

---
+++ hw2/hw2_104753017.R
@@ -0,0 +1,98 @@
+######################################
+# the reference code of program2 
+######################################
+
+######################################
+# initial
+######################################
+library("Biostrings",verbose=F,quietly=T)
+
+# # read parameters
+# args = commandArgs(trailingOnly=TRUE)
+# if (length(args)==0) {
+#   stop("USAGE: Rscript pro2_<your student ID>.R --input test.fasta --score pam250.txt --aln global --gap_open -10 --gap_extend -2 --output result.fasta", call.=FALSE)
+# }
+# 
+# # parse parameters
+# i<-1 
+# while(i < length(args))
+# {
+#   if(args[i] == "--input"){
+#     i_f<-args[i+1]
+#     i<-i+1
+#   }else if(args[i] == "--score"){
+#     s_f<-args[i+1]
+#     i<-i+1
+#   }else if(args[i] == "--aln"){
+#     aln_mode <- args[i+1]
+#     i<-i+1
+#   }else if(args[i] == "--gap_open"){
+#     g_o<- as.integer(args[i+1]) 
+#     i<-i+1
+#   }else if(args[i] == "--gap_extend"){
+#     g_e<-as.integer(args[i+1])
+#     i<-i+1    
+#   }else if(args[i] == "--output"){
+#     o_f<-args[i+1]
+#     i<-i+1
+#   }else{
+#     stop(paste("Unknown flag", args[i]), call.=FALSE)
+#   }
+#   i<-i+1
+# }
+
+i_f = "C:/Users/dcfdcm/Desktop/bioinformatics/hw2/test.fasta"
+o_f = "C:/Users/dcfdcm/Desktop/bioinformatics/hw2/result.fasta"
+s_f = "C:/Users/dcfdcm/Desktop/bioinformatics/hw2/pam250.txt"
+g_o = 10
+g_e = 2
+#aln_mode = global
+
+print("PARAMETERS")
+print(paste("input file         :", i_f))
+print(paste("output file        :", o_f))
+print(paste("score file         :", s_f))
+print(paste("gap open penalty   :", g_o))
+print(paste("gap extend penalty :", g_e))
+
+######################################
+# main
+######################################
+# read fasta file
+ff <- readAAStringSet(i_f)
+seq_name = names(ff)
+sequence = paste(ff)
+
+# aln length
+aln_length1 <- nchar(sequence[1])
+aln_length2 <- nchar(sequence[2])
+
+# read score file
+s_m<-read.table(s_f)
+s_m<-as.matrix(s_m)
+
+#matrix
+m = matrix(0, nrow=aln_length2+1, ncol=aln_length1+1, byrow=T)
+
+
+
+aln_score<-0
+for(i in 1:aln_length1)
+{
+  a<-substring(sequence[1], i, i)
+  b<-substring(sequence[2], i, i)
+  
+  if((a != "-")&&(b != "-"))
+  {
+    print(paste(a, "-", b, "=", s_m[a,b]))
+    aln_score = aln_score + s_m[a,b]
+  }
+  else{
+    aln_score = aln_score + g_o
+  }
+}
+
+print(aln_score)
+
+# output
+#writeXStringSet(ff, o_f)
